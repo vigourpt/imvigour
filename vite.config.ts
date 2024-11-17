aa033ca20d0b3783295c4afb9fb20f2efed31602
vigourpt <vigourpt@googlemail.com>
2024-11-17 15:08:30 +0000

    Updated vite.config.ts and fixed case mismatch in output file names

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name][extname]',
+      },
+    },
+  },
 });
