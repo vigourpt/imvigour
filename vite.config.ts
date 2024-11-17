1757563e35cdea2ea293f5f387b7a1d7cc43e957
vigourpt <vigourpt@googlemail.com>
2024-11-17 15:15:42 +0000

    Fix rollup output filenames and rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Adjust the base as needed
   plugins: [react()],
   build: {
     rollupOptions: {
